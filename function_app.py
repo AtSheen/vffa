bbd24cb2d0e67a959749379204d254da2d083c17
AtSheen <158444970+AtSheen@users.noreply.github.com>
2024-12-11 14:20:14 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,10 +1,17 @@
 import azure.functions as func
 import logging
-from azure.storage.blob import BlobServiceClient
+
 
 @app.route(route="httpget", methods=["GET"])
 def http_get(req: func.HttpRequest) -> func.HttpResponse:
 
+    logging.info(f"Processing GET request. Name:")
+
+    return func.HttpResponse(f"Hello,!")
+
+@app.route(route="httpget2", methods=["GET"])
+def http_get(req: func.HttpRequest) -> func.HttpResponse:
+    from azure.storage.blob import BlobServiceClient
     logging.info(f"Processing GET request. Name:")
 
     return func.HttpResponse(f"Hello,!")
